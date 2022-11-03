42ef8697f53423f055b3df79a33b32da8d4e62a3
EMTac <115601690+EMTac@users.noreply.github.com>
2022-11-02 20:00:18 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,64 @@
+var map = L.map('map').setView([47.25, -122.44], 11);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/navigation-night-v1',
+    accessToken: 'pk.eyJ1IjoiZW10YWMiLCJhIjoiY2w5ejR0bXZyMGJpbDNvbG5jMTFobGJlZCJ9.UMi2J2LPPuz0qbFaCh0uRA',
+}).addTo(map);
+
+L.Control.textbox = L.Control.extend({
+		onAdd: function(map) {
+		var text = L.DomUtil.create('div', 'info-text');
+    text.id = "info_text";
+		text.innerHTML = "<strong>Click on the map to select start and end points for routing or use the navigation pane to enter location addresses</strong>";
+    return text;
+		},
+
+		onRemove: function(map) {
+			// Nothing to do here
+		}
+	});
+	L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+	L.control.textbox().addTo(map);
+
+var control = L.Routing.control({
+    geocoder: L.Control.Geocoder.photon(),
+    waypoints: [
+        null
+        //L.latLng(47.246587, -122.438830),
+        //L.latLng(47.318017, -122.542970)
+    ],
+    router: L.Routing.mapbox('pk.eyJ1IjoiZW10YWMiLCJhIjoiY2w5ejR0bXZyMGJpbDNvbG5jMTFobGJlZCJ9.UMi2J2LPPuz0qbFaCh0uRA'),
+    units:'imperial',
+    show: false,
+    collapsible: true,
+    routeWhileDragging: true
+}).addTo(map);
+
+function createButton(label, container) {
+    var btn = L.DomUtil.create('button', '', container);
+    btn.setAttribute('type', 'button');
+    btn.innerHTML = label;
+    return btn;
+}
+
+map.on('click', function(e) {
+    var container = L.DomUtil.create('div'),
+        startBtn = createButton('Start from this location', container),
+        destBtn = createButton('Go to this location', container);
+
+    L.popup()
+        .setContent(container)
+        .setLatLng(e.latlng)
+        .openOn(map);
+    L.DomEvent.on(startBtn, 'click', function() {
+        control.spliceWaypoints(0, 1, e.latlng);
+        map.closePopup();
+    });
+    L.DomEvent.on(destBtn, 'click', function() {
+        control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+        control.show();
+        map.closePopup();
+    });
+ });
